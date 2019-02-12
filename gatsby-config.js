008fb1e3a8270817903db827095cb2a8d54e4fc4
Carl Aiau <carl@ortus.co.nz>
2019-02-12 23:12:48 +1300

    test build with purgeCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,9 +45,19 @@ module.exports = {
 				anonymize: true
 			}
 		},
-		`gatsby-plugin-sitemap`
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.app/offline
-		// 'gatsby-plugin-offline',
+		{ 
+			resolve: `gatsby-plugin-purgecss`,
+			options: {
+				printRejected: true, // Print removed selectors and processed file names
+				// develop: true, // Enable while using `gatsby develop`
+				// tailwind: true, // Enable tailwindcss support
+				// whitelistPatterns: [/visible@l/, /hidden@l/, /uk-width\-\d\-\d@l$/],
+				// ignore: ['/components/less/layout.less'], // Ignore files/folders
+				// purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+			}
+		}
+		
+		
+		
 	]
 };
